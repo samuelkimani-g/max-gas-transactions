390a6814e6ac824203f3bb8c00101388c5dbb91c
samuelkimani-g <samuelgichuru94@gmail.com>
2025-07-17 19:01:24 +0300

    Simplify Vercel config to focus on static files and fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}
-)
+})
